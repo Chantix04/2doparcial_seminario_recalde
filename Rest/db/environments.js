66591ded9995dc2989a797b1f683036a43555787
Chantix04 <gstnrcld@gmail.com>
2023-11-27 10:58:58 -0300

    segundo intento

--- Rest/db/environments.js
+++ Rest/db/environments.js
@@ -5,7 +5,7 @@ export const environments = {
   SECRET: process.env.SECRET || 'my_secret',
   DB: {
     DB_NAME: process.env.DB_NAME || 'swarmdb',
-    DB_HOST: process.env.DB_HOST || 'mysql',
+    DB_HOST: process.env.DB_HOST || 'mysql-service',
     DB_DIALECT: process.env.DB_DIALECT || 'mysql',
     DB_USER: process.env.DB_USER || 'root',
     DB_PASSWORD: process.env.DB_PASSWORD || 'root',
